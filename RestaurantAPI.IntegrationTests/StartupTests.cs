125f028cafe1d1c744d1ec76544e2675d8e0e67d
simonbws <simonbws6@gmail.com>
2024-06-07 14:36:25 +0200

    testing registeration of dependencies

---
+++ RestaurantAPI.IntegrationTests/StartupTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Controllers;
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Startup>> //typ IClassFixture dla typu WebAppFactory dla kasy Startup
+    {
+        private readonly List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Startup> _factory;
+        public StartupTests(WebApplicationFactory<Startup> factory)
+        {
+              _controllerTypes = typeof(Startup)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    _controllerTypes.ForEach(c=>services.AddScoped(c));
+                });
+
+            });
+        }
+
+        [Fact]
+        public void ConfigureServices_ForControllers_RegistersAllDependencies()
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+
+
+            //assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+
+            });
+
+        }
+    }
+}

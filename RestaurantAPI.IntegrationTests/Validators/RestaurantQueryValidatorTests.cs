cfc51aa21837dce51baf6dcc078fe4b5dd4f14b9
simonbws <simonbws6@gmail.com>
2025-09-26 14:32:09 +0200

    RestaurantQueryValidator test

---
+++ RestaurantAPI.IntegrationTests/Validators/RestaurantQueryValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantAPI.Models.Validators;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.IntegrationTests.Validators
+{
+    public class RestaurantQueryValidatorTests
+    {
+        public void Validate_ForCorrectModel_ReturnsSuccess(RestaurantQuery model)
+        {
+            // arrange
+            var validator = new RestaurantQueryValidator();
+
+
+            // act
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}

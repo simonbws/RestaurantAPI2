d88671d3e5aaacd4034e3e77015c9a9f1c5735e1
simonbws <simonbws6@gmail.com>
2025-09-26 20:29:10 +0200

    Restaurant Query for (In)Correct model tests

--- RestaurantAPI.IntegrationTests/Validators/RestaurantQueryValidatorTests.cs
+++ RestaurantAPI.IntegrationTests/Validators/RestaurantQueryValidatorTests.cs
@@ -1,11 +1,93 @@
 ﻿using RestaurantAPI.Models.Validators;
 using RestaurantAPI.Models;
+using Xunit;
+using FluentValidation.TestHelper;
+using System.Collections;
+using RestaurantAPI.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace RestaurantAPI.IntegrationTests.Validators
 {
     public class RestaurantQueryValidatorTests
     {
+        public static IEnumerable<object[]> GetSampleValidData()
+        {
+            var list = new List<RestaurantQuery>()
+            {
+             new RestaurantQuery()
+            {
+                PageNumber = 1,
+                PageSize = 10
+            },
+            new RestaurantQuery()
+            {
+                PageNumber = 2,
+                PageSize = 15
+            },
+            new RestaurantQuery()
+            {
+                PageNumber = 22,
+                PageSize = 5,
+                SortBy = nameof(Restaurant.Name),
+            },
+            new RestaurantQuery()
+            {
+                PageNumber = 12,
+                PageSize = 15,
+                SortBy = nameof(Restaurant.Category)
+            }
+        };
+            return list.Select(q => new object[] { q });
+
+        }
+        public static IEnumerable<object[]> GetSampleInvalidData()
+        {
+            var list = new List<RestaurantQuery>()
+            {
+                 new RestaurantQuery()
+                {
+                    PageNumber = 0,
+                    PageSize = 10
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 2,
+                    PageSize = 13
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 22,
+                    PageSize = 5,
+                    SortBy = nameof(Restaurant.ContactEmail)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber = 12,
+                    PageSize = 15,
+                    SortBy = nameof(Restaurant.ContactNumber)
+                }
+            };
+            return list.Select(q => new object[] { q });
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleValidData))]
         public void Validate_ForCorrectModel_ReturnsSuccess(RestaurantQuery model)
+        {
+            // arrange
+            var validator = new RestaurantQueryValidator();
+
+            // act
+            var result = validator.TestValidate(model);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleInvalidData))]
+        public void Validate_ForIncorrectModel_ReturnsFailure(RestaurantQuery model)
         {
             // arrange
             var validator = new RestaurantQueryValidator();
@@ -15,7 +97,7 @@ namespace RestaurantAPI.IntegrationTests.Validators
             var result = validator.TestValidate(model);
 
             // assert
-            result.ShouldNotHaveAnyValidationErrors();
+            result.ShouldHaveAnyValidationError();
         }
     }
 }
